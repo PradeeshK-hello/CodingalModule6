4d24872524929f14cf8865e533524d58abe3b783
PradeeshK-hello <sp.pradeeshkumar@gmail.com>
2026-02-18 19:41:29 +0800

    Add files via upload

---
+++ ArrayLengthRecursive.py
@@ -0,0 +1,7 @@
+def lengthArray(arr):
+    if len(arr) == 1:
+        return 1
+    return len(arr)
+arr = [1,2,234,234,745,3,6,653]
+print("Array:",arr)
+print("The length of the given array is:",lengthArray(arr))
